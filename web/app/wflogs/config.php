8eb175f4682658e7c1737158f1ca6a6532c6090d
deangelisp <deangelissp@gmail.com>
2020-12-13 23:23:33 +1300

    First Message

---
+++ web/app/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1606435851;s:7:"authKey";s:64:"oy.fO1cZG3W>dkGM0fR/R3+1l9[XRsA#.)Bo5^K)r(GD2w ?zJ(RkjG!.T8K{0Sq";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3465;}
